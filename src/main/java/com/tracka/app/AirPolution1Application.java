8a9cc65c25c7311d356c938837f975f4bd6aa71c
Muhire-Josue <rutayisire.josue09@gmail.com>
2020-09-13 13:20:47 +0200

    initial commit

---
+++ src/main/java/com/tracka/app/AirPolution1Application.java
@@ -0,0 +1,13 @@
+package com.tracka.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirPolution1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirPolution1Application.class, args);
+	}
+
+}
