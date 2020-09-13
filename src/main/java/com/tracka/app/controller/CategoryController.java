1e359f2d676a631c66f75db5ce33aedac0374904
Muhire-Josue <rutayisire.josue09@gmail.com>
2020-09-13 17:00:28 +0200

    add category controller

--- src/main/java/com/tracka/app/controller/CategoryController.java
+++ src/main/java/com/tracka/app/controller/CategoryController.java
@@ -3,6 +3,7 @@ package com.tracka.app.controller;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,7 @@ public class CategoryController {
 	@Autowired
 	CategoryRepository categoryRepository;
 	
+	@GetMapping
 	public Collection<Category> categories(){
 		return categoryRepository.findAll();
 	}
