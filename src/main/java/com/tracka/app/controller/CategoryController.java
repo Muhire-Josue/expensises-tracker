bbecca4d5f82d9de391759792ede4aa6395e8338
Muhire-Josue <rutayisire.josue09@gmail.com>
2020-09-13 17:09:33 +0200

    add category controller

--- src/main/java/com/tracka/app/controller/CategoryController.java
+++ src/main/java/com/tracka/app/controller/CategoryController.java
@@ -1,9 +1,13 @@
 package com.tracka.app.controller;
 
 import java.util.Collection;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,9 +20,17 @@ public class CategoryController {
 
 	@Autowired
 	CategoryRepository categoryRepository;
-	
-	@GetMapping
-	public Collection<Category> categories(){
+
+	@GetMapping("/category")
+	public Collection<Category> categories() {
 		return categoryRepository.findAll();
+	}
+
+	@GetMapping("/category/{id}")
+	public ResponseEntity<?> getCategory(@PathVariable long id) {
+		Optional<Category> category = categoryRepository.findById(id);
+
+		return category.map(response -> ResponseEntity.ok().body(response))
+				.orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
 	}
 }
