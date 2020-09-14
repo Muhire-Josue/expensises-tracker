9f1bd7d2376cf9ae7aed6478ec19469273ea0ea2
Muhire-Josue <rutayisire.josue09@gmail.com>
2020-09-14 12:38:29 +0200

    add categories react client

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
-import NavBar from './NavBar';
-import {} from ''
+import Categories from './Category';
+import Expenses from './Expense';
+import {Route, BrowserRouter as Router, Switch} from 'react-router-dom'
 
 function App() {
   return (
-    <div>
-      <NavBar />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/categories" exact={true} component={Categories}/>
+        <Route path="/expenses" exact={true} component={Expenses}/>
+      </Switch>
+    </Router>
   );
 }
 
