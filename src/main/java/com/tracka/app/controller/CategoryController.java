15caf2b2388914b456eb2d9a1f1dcf35b7837ea4
Muhire-Josue <rutayisire.josue09@gmail.com>
2020-09-14 10:05:07 +0200

    add category controller

--- src/main/java/com/tracka/app/controller/CategoryController.java
+++ src/main/java/com/tracka/app/controller/CategoryController.java
@@ -6,8 +6,12 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,5 +36,26 @@ public class CategoryController {
 
 		return category.map(response -> ResponseEntity.ok().body(response))
 				.orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+	}
+	
+	@PostMapping("/category")
+	public Category createCategory(@RequestBody Category category){
+		Category result = categoryRepository.save(category);
+		
+		return result;
+	}
+	
+	@PutMapping("/category")
+	public ResponseEntity<Category> updateCategory(@RequestBody Category category){
+		Category updatedCategory = categoryRepository.save(category);
+		return ResponseEntity.ok().body(updatedCategory);
+	}
+	
+	@DeleteMapping("/category/{id}")
+	
+	public ResponseEntity<?> deleteCategory(@PathVariable long id){
+		categoryRepository.deleteById(id);
+		
+		return ResponseEntity.ok().build();
 	}
 }
--- src/main/java/com/tracka/app/model/Category.java
+++ src/main/java/com/tracka/app/model/Category.java
@@ -1,23 +1,32 @@
 package com.tracka.app.model;
 
-import javax.persistence.CascadeType;
+import java.io.Serializable;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@NoArgsConstructor
 @Entity
-@Data
 @Table(name = "categories")
-public class Category {
-
+public class Category implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	private String name;
-
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 }
