983c61b6befb170373a09f0f306f0ae9714a51c9
Muhire-Josue <rutayisire.josue09@gmail.com>
2020-09-14 11:14:46 +0200

    add categories react client

---
+++ client/src/Category.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+
+export class Category extends Component {
+    state = {
+        isLoading: true,
+        categories: []
+    }
+    async componentDidMount(){
+        const response = await fetch('http://localhost:8080/api/category');
+        const body = await response.json();
+        this.setState({categories: body, isLoading: false});
+    }
+    render() {
+        const {isLoading, categories} = this.state;
+        if(isLoading){
+            return (<div>Loading.....</div>)
+        }
+        return (
+           <div>
+               <h2>Categories</h2>
+               {
+                   categories.map(category => 
+                    <div id={category.id}>
+                        {category.name}
+                    </div>
+                    )
+               }
+           </div>
+        )
+    }
+}
+
+export default Category
